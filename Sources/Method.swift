143a8d7b663259d043b12cac2ff9d3561d2d1d82
Dan Neumann <danrodneu@gmail.com>
2016-02-28 18:36:56 -0600

    Flesh out methods

--- Sources/Method.swift
+++ Sources/Method.swift
@@ -1,13 +1,37 @@
 
 import Foundation
 
-// TODO: Support all methods
-
 public enum Method: String {
+  // Common
   case Get = "GET"
   case Post = "POST"
   case Put = "PUT"
-  case Delete = "DELETE"
   case Head = "HEAD"
+  case Delete = "DELETE"
+  case Options = "OPTIONS"
+  case Patch = "PATCH"
+
+  // Uncommon
+  case Trace = "TRACE"
+  case Copy = "COPY"
+  case Lock = "LOCK"
+  case Mkcol = "MKCOL"
+  case Move = "MOVE"
+  case Purge = "PURGE"
+  case Propfind = "PROPFIND"
+  case Proppatch = "PROPPATCH"
+  case Unlock = "UNLOCK"
+  case Report = "REPORT"
+  case Mkactivity = "MKACTIVITY"
+  case Checkout = "CHECKOUT"
+  case Merge = "MERGE"
+  case Msearch = "M-SEARCH"
+  case Notify = "NOTIFY"
+  case Subscribe = "SUBSCRIBE"
+  case Unsubscribe = "UNSUBSCRIBE"
+  case Search = "SEARCH"
+  case Connect = "CONNECT"
+
+  // Internal
   case Unknown = "_"
 }
