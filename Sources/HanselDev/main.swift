b50946e6d90530f6e51d11e97ff8982d8c095c32
Dan Neumann <danrodneu@gmail.com>
2016-03-05 20:30:32 -0600

    Update main.swift demo

--- Sources/HanselDev/main.swift
+++ Sources/HanselDev/main.swift
@@ -1,60 +1,57 @@
 
 import Hansel
-import Jay
 
 let middleware: Middleware = compose(
   Batteries.logger,
   Batteries.removeTrailingSlash,
-  Batteries.notModified(etag: true),
-  Batteries.serveStatic("/Users/danneu/Code/Swift/Hansel/Public")
+  Batteries.notModified(etag: false)
 )
 
-struct Message: JsonEncodable {
-  let id: Int
-  let uname: String
-  let text: String
-
-  init (_ id: Int, _ uname: String, _ text: String) {
-    self.id = id
-    self.uname = uname
-    self.text = text
-  }
-
-  static let decoder: Decoder<Message> =
-    JD.object3(
-      Message.init,
-      "id" => JD.int,
-      ["user", "uname"] => JD.string,
-      "text" => JD.string
-    )
-
-  func json () -> JsonValue {
-    return .Object(
-      ["id": .Number(.JsonInt(id)),
-       "user": .Object(["uname": .String(uname)]),
-       "text": .String(text)])
-  }
+func demoTemplate (ip: String) -> HtmlConvertible {
+  return div(
+    h1("Welcome!"),
+    p(["style": ["color": "red"]],
+      "Your IP address is: ", strong(ip)))
 }
 
-//let handler: Handler = { req in
-//  let msg = try req.json(Message.decoder)
-//  print("msg", msg)
-//  return try Response().json(msg)
-//}
+let router: Router = .Node("/", [
+  // curl http://localhost:4000
+  .Route(.Get, { req in
+    return Response().text("Welcome to the homepage")
+  }),
+  // curl http://localhost:4000/html
+  .Node("/html", [
+    .Route(.Get, { req in
+      return Response().html(demoTemplate(req.ip))
+    })
+  ]),
+  // curl http://localhost:4000/json-encode
+  .Node("/json-encode", [
+    .Route(.Get, { req in
+     return try Response().json(["hello": 42])
+    })
+  ]),
+  // curl -H 'Content-Type: application/json' -d '{"uname":"chuck","password":"secret"}' http://localhost:4000/json-decode
+  .Node("/json-decode", [
+    .Route(.Post, { req in
+      // decodes json {"uname": String, "password": String} => (String, String)
+      let decoder = JD.object2({ ($0, $1) }, "uname" => JD.string, "password" => JD.string)
+      let (uname, password) = try req.json(decoder)
+      // TODO: look up credentials...
+      return Response().text("You logged in as \(uname)")
+    })
+  ]),
+  // curl -H 'Content-Type: application/json' -d '[1, 2, 3]' http://localhost:4000/json-sum
+  .Node("/json-sum", [
+    .Route(.Post, { req in
+      let nums = try req.json(JD.array(JD.int))
+      let sum = nums.reduce(0, combine: +)
+      return try Response().json(["sum": sum])
+    })
+  ])
+])
 
-//let handler: Handler = { req in
-//  let nums = try req.json(JD.array(JD.int))
-//  let sum = nums.reduce(0, combine: +)
-//  return try Response().json(["sum": sum])
-//}
+let rootHandler: Handler = middleware(router.handler())
 
-let handler: Handler = { req in
-  return Response().text("ok")
-  //Response().html(d.p("Hello world"))
-  //let charset = req.charset ?? "--"
-  //return Response().html(p("charset: \(charset)"))
-}
+Server(rootHandler).listen(4000)
 
-let root: Handler = middleware(handler)
-
-Server(root).listen(4001)
